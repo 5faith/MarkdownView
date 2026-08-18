388e8c8b3ba487e43329201a0173888567555c50
5faith <5faith@users.noreply.github.com>
2026-08-18 11:57:57 +0800

    fix: handle Ctrl+C gracefully to avoid STATUS_CONTROL_C_EXIT error

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -6,6 +6,14 @@ pub fn run() {
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_process::init())
+        .setup(|app| {
+            let handle = app.handle().clone();
+            ctrlc::set_handler(move || {
+                handle.exit(0);
+            })
+            .expect("Error setting Ctrl-C handler");
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
